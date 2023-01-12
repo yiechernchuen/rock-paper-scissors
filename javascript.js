b4c85c945e8e1ce5465cb14daf987e3e45fcf75e
yiechernchuen <yccgithub@gmail.com>
2023-01-12 15:34:01 +0800

    Added user prompt and play function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let randomNumber = Math.floor(Math.random() * (4 - 1) + 1);
+let playerSelection = prompt('Please enter "Rock","Paper", or "Scissors"', "");
 
 function getComputerChoice(input) {
   if (input === 1) {
@@ -6,8 +7,34 @@ function getComputerChoice(input) {
   } else if (input === 2) {
     return "paper";
   } else {
-    return "scissor";
+    return "scissors";
   }
 }
 
 console.log(getComputerChoice(randomNumber));
+
+function play(playerChoice, computerSelection) {
+  if (playerChoice == "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerChoice == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerChoice == "rock" && computerSelection == "rock") {
+    return "Its a Draw!";
+  } else if (playerChoice == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerChoice == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerChoice == "paper" && computerSelection == "paper") {
+    return "Its a Draw!";
+  } else if (playerChoice == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerChoice == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerChoice == "scissors" && computerSelection == "scissors") {
+    return "Its a Draw!";
+  } else {
+    return "Invalid input.";
+  }
+}
+
+alert(play(playerSelection, getComputerChoice(randomNumber)));

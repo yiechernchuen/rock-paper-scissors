ccf7d132240df9e7ed668c604f50ba4cbc966529
yiechernchuen <yccgithub@gmail.com>
2023-02-28 18:49:12 +0800

    Improve code readability

--- javascript.js
+++ javascript.js
@@ -13,71 +13,69 @@ let drawRound = 0;
 
 const computerChoice = ['ROCK', 'PapER', 'Scissors'];
 
-function getComputerChoice(input) {
-  return computerChoice[input].toLowerCase();
-}
+const getComputerChoice = (input) => computerChoice[input].toLowerCase();
 
-function playRound(playerSelection, computerSelection) {
+const playRound = (playerSelection, computerSelection) => {
   let victor;
-  if (playerSelection == 'rock') {
-    if (computerSelection == 'paper') {
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
       alert('You Lose! Paper beats Rock');
-      victor = 'Computer';
-    } else if (computerSelection == 'scissors') {
+      victor = 'computer';
+    } else if (computerSelection === 'scissors') {
       alert('You Win! Rock beats Scissors');
-      victor = 'Player';
-    } else if (computerSelection == 'rock') {
+      victor = 'player';
+    } else if (computerSelection === 'rock') {
       alert('Its a Draw!');
     }
-  } else if (playerSelection == 'paper') {
-    if (computerSelection == 'rock') {
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
       alert('You Win! Paper beats Rock');
-      victor = 'Player';
-    } else if (computerSelection == 'scissors') {
+      victor = 'player';
+    } else if (computerSelection === 'scissors') {
       alert('You Lose! Scissors beats Paper');
-      victor = 'Computer';
-    } else if (computerSelection == 'paper') {
+      victor = 'computer';
+    } else if (computerSelection === 'paper') {
       alert('Its a Draw!');
     }
-  } else if (playerSelection == 'scissors') {
-    if (computerSelection == 'rock') {
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
       alert('You Lose! Rock beats Scissors');
-      victor = 'Computer';
-    } else if (computerSelection == 'paper') {
+      victor = 'computer';
+    } else if (computerSelection === 'paper') {
       alert('You Win! Scissors beats Paper');
-      victor = 'Player';
-    } else if (computerSelection == 'scissors') {
+      victor = 'player';
+    } else if (computerSelection === 'scissors') {
       alert('Its a Draw!');
     }
   } else {
     alert('Invalid input.');
   }
   calculateResult(victor);
-}
+};
 
-function game() {
-  for (let i = 0, playerSelection, randomNumber, wins; i < 5; i++) {
-    playerSelection = prompt('Please enter "Rock","Paper", or "Scissors"', '').toLowerCase();
+const calculateResult = (victor) => {
+  if (victor === 'computer') {
+    computer += 1;
+  } else if (victor === 'player') {
+    player += 1;
+  } else {
+    drawRound += 1;
+  }
+};
+
+const game = () => {
+  let playerSelection = '';
+  let randomNumber = 0;
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt('Please enter "Rock","Paper", or "Scissors"').toLowerCase();
     console.log(playerSelection);
     randomNumber = Math.floor(Math.random() * (3 - 1) + 1);
     console.log(getComputerChoice(randomNumber));
     playRound(playerSelection, getComputerChoice(randomNumber));
   }
-}
+};
 
 game();
-
-function calculateResult(victor) {
-  victor = victor.toLowerCase();
-  if (victor == 'computer') {
-    computer = computer + 1;
-  } else if (victor == 'player') {
-    player = player + 1;
-  } else {
-    drawRound = drawRound + 1;
-  }
-}
-
-if (computer + player + drawRound == 5) {
-  alert('You have won ' + player + ' out of 5 games');
+if (computer + player + drawRound === 5) {
+  alert(`You have won ${player} out of 5 games`);
 }

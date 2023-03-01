b7d2d8c63112632e25da1e1ae87f1bf48f2ea2bf
yiechernchuen <yccgithub@gmail.com>
2023-03-01 14:34:54 +0800

    Added html and css elements, modified js based on TOP criteria

--- javascript.js
+++ javascript.js
@@ -4,33 +4,33 @@ const playRound = (playerSelection, computerSelection) => {
   let victor = '';
   if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-      alert('You Lose! Paper beats Rock');
+      paragraph.textContent = 'You Lose! Paper beats Rock';
       victor = 'computer';
     } else if (computerSelection === 'scissors') {
-      alert('You Win! Rock beats Scissors');
+      paragraph.textContent = 'You Win! Rock beats Scissors';
       victor = 'player';
     } else if (computerSelection === 'rock') {
-      alert('Its a Draw!');
+      paragraph.textContent = 'Its a Draw!';
     }
   } else if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
-      alert('You Win! Paper beats Rock');
+      paragraph.textContent = 'You Win! Paper beats Rock';
       victor = 'player';
     } else if (computerSelection === 'scissors') {
-      alert('You Lose! Scissors beats Paper');
+      paragraph.textContent = 'You Lose! Scissors beats Paper';
       victor = 'computer';
     } else if (computerSelection === 'paper') {
-      alert('Its a Draw!');
+      paragraph.textContent = 'Its a Draw!';
     }
   } else if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
-      alert('You Lose! Rock beats Scissors');
+      paragraph.textContent = 'You Lose! Rock beats Scissors';
       victor = 'computer';
     } else if (computerSelection === 'paper') {
-      alert('You Win! Scissors beats Paper');
+      paragraph.textContent = 'You Win! Scissors beats Paper';
       victor = 'player';
     } else if (computerSelection === 'scissors') {
-      alert('Its a Draw!');
+      paragraph.textContent = 'Its a Draw!';
     }
   } else {
     alert('Invalid input.');
@@ -41,18 +41,31 @@ const playRound = (playerSelection, computerSelection) => {
 const calculateResult = (victor) => {
   if (victor === 'computer') {
     computer += 1;
+    computerScore.textContent = computer;
   } else if (victor === 'player') {
     player += 1;
+    playerScore.textContent = player;
   } else {
     drawRound += 1;
   }
+  if (player === 5) {
+    paragraph.textContent = 'You have won 5 games!';
+    player = 0;
+    computer = 0;
+    computerScore.textContent = computer;
+    playerScore.textContent = player;
+  } else if (computer === 5) {
+    paragraph.textContent = 'The PC has won 5 games!';
+    player = 0;
+    computer = 0;
+    computerScore.textContent = computer;
+    playerScore.textContent = player;
+  }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('Please enter "Rock","Paper", or "Scissors"').toLowerCase();
-    playRound(playerSelection, getComputerChoice(Math.floor(Math.random() * 3)));
-  }
+const game = (e) => {
+  let playerChoice = e.target.textContent;
+  playRound(playerChoice, getComputerChoice(Math.floor(Math.random() * 3)));
 };
 
 let computer = 0;
@@ -61,7 +74,13 @@ let drawRound = 0;
 
 const computerChoice = ['ROCK', 'PapER', 'Scissors'];
 
-game();
-if (computer + player + drawRound === 5) {
-  alert(`You have won ${player} out of 5 games`);
-}
+const playerScore = document.querySelector('.playerScore');
+const computerScore = document.querySelector('.computerScore');
+
+const paragraph = document.querySelector('p');
+
+const button = document.querySelectorAll('button');
+
+button.forEach((button) => {
+  button.addEventListener('click', game);
+});
